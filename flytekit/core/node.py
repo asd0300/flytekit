d6db854bd7d9a21ad6d47e22a6018b5806ff0f88
范博雅 <fan0300@gmail.com>
2025-01-22 06:10:49 +0800

    test

--- flytekit/core/node.py
+++ flytekit/core/node.py
@@ -197,6 +197,7 @@ class Node(object):
         *args,
         **kwargs,
     ):
+        #
         if secret_requests is not None:
             assert_not_promise(secret_requests, "secret_requests")
             self._secret = secret_requests
